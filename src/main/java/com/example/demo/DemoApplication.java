f3c1b7c31dd339975c8594bebe974531d3897fdd
silenc3502 <gcccompil3r@gmail.com>
2020-07-03 18:00:45 +0900

    Add Comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	// SpringApplication.run 하면
+	// 웹 서버가 구동된다 - 서비스 번호: 8080
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -10,10 +10,13 @@ public class MainController {
     static final Logger log =
             LoggerFactory.getLogger(MainController.class);
 
+    // localhost:8080/으로 접속하면
+    // 제어 유닛이 아래 코드를 실행시킨다.
     @GetMapping("/")
     public String index() {
         log.info("index()");
 
+        // index.html을 보여준다.
         return "index";
     }
 }
