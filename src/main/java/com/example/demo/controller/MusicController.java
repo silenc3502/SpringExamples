3c3c57fa1f8d9c0e43aa8458c8449dad304e2a4e
silenc3502 <gcccompil3r@gmail.com>
2020-07-23 16:02:59 +0900

    Spring Params

--- src/main/java/com/example/demo/controller/MusicController.java
+++ src/main/java/com/example/demo/controller/MusicController.java
@@ -1,29 +1,50 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Music;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import java.lang.reflect.Member;
+
+@RestController
 @RequestMapping("musics")
 public class MusicController {
     final static Logger log =
             LoggerFactory.getLogger(MusicController.class);
 
     @GetMapping("")
-    public String musicIndex() {
+    public ModelAndView musicIndex() {
         log.info("musicIndex()");
 
-        return "music/musicIndex";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("music/musicIndex");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/test")
+    public Music musicTest() {
+        log.info("musicTest()");
+
+        Music music = new Music();
+
+        return music;
     }
 
     @GetMapping("/play")
-    public String musicPlay() {
+    public ModelAndView musicPlay() {
         log.info("musicPlay()");
 
-        return "music/musicPlay";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("music/musicPlay");
+
+        return modelAndView;
     }
 
     @GetMapping("/menu")
---
+++ src/main/java/com/example/demo/entity/Music.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+public class Music {
+    private String writer = "sdr4linux";
+    private String title = "go go go";
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
