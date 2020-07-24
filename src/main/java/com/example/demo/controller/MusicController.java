d04016b1fcb7885852688890c55988771d15820f
silenc3502 <gcccompil3r@gmail.com>
2020-07-24 12:11:04 +0900

    Spring Params

--- src/main/java/com/example/demo/controller/MusicController.java
+++ src/main/java/com/example/demo/controller/MusicController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Music;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// @Controller는 HTML 핸들링에 특화되어 있으므로
+// 데이터를 처리하고자 할 경우 ResponseEntity를 활용해줘야 한다.
+// @RestController는 json 형식으로 데이터 처리에 특화되어 있으므로
+// HTML을 처리하고자 할 경우 ModelAndView를 활용해야 한다.
 @RestController
 @RequestMapping("musics")
 public class MusicController {
@@ -118,5 +127,59 @@ public class MusicController {
         return new ResponseEntity<String>(
                 "Success", HttpStatus.OK
         );
+    }
+
+    @GetMapping("/respclass")
+    public ResponseEntity<Music> musicRespClass() {
+        log.info("musicRespClass()");
+
+        Music music = new Music();
+
+        return new ResponseEntity<Music>(music, HttpStatus.OK);
+    }
+
+    @GetMapping("/resplist")
+    public ResponseEntity<List<Music>> musicRespList() {
+        log.info("musicRespList()");
+
+        List<Music> list = new ArrayList<Music>();
+
+        Music music1 = new Music();
+        list.add(music1);
+
+        Music music2 = new Music();
+        list.add(music2);
+
+        return new ResponseEntity<List<Music>>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("respfile")
+    public ResponseEntity<byte[]> musicImgFile()
+                                    throws Exception {
+        log.info("musicImgFile()");
+
+        InputStream in = null;
+        ResponseEntity<byte[]> entity = null;
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            in = new FileInputStream("");
+            headers.setContentType(MediaType.IMAGE_JPEG);
+
+            entity = new ResponseEntity<byte[]>(
+                    IOUtils.toByteArray(in),
+                    headers,
+                    HttpStatus.CREATED
+            );
+        } catch(Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<byte[]>(
+                    HttpStatus.BAD_REQUEST);
+        } finally {
+            in.close();
+        }
+
+        return entity;
     }
 }
