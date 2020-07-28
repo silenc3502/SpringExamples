78ecff5b8d7c91d111bd297c0461481da40b44f3
silenc3502 <gcccompil3r@gmail.com>
2020-07-28 10:20:42 +0900

    Vue First

---
+++ src/main/java/com/example/demo/controller/VueTestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VueTestController {
+
+    @GetMapping("/vuetest")
+    public String vueIndex() {
+        return "vuetest/vueIndex";
+    }
+}
