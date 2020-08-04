0371819adb02e495b09c7b10a8fa985b96a122c1
silenc3502 <gcccompil3r@gmail.com>
2020-08-04 10:28:20 +0900

    Vue-CLI Second

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -28,6 +28,30 @@
       <p>{{ message }}</p>
       <input v-model="message"><br>
       <button @click="incCnt">{{ cnt }}</button><br>
+      <!-- border-collapse는 테두리와 셀 사이 간격 삭제 -->
+      <p>count 기반 순회</p>
+      <table border="1" style="border-collapse:collapse;">
+        <tr v-bind:key=idx v-for="idx in count">
+          <td>{{ msg }}</td>
+          <td>{{ idx }}</td>
+        </tr>
+      </table>
+
+      <p>list() 기반 순회</p>
+      <table border="1" style="border-collapse:collapse;">
+        <tr v-bind:key=idx v-for="idx in list()">
+          <td>{{ msg }}</td>
+          <td>{{ idx }}</td>
+        </tr>
+      </table>
+
+      <p>(key, value) 기반 순회</p>
+      <table border="1" style="border-collapse:collapse;">
+        <tr v-bind:key="(value, key)" v-for="(value, key) in data">
+          <td>{{ value }}</td>
+          <td>{{ key }}</td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -41,6 +65,15 @@ Vue.use(cookies)
 export default {
   data: function () {
     return {
+      msg: 'Test',
+      count: 7,
+      list: function () {
+        var list = []
+        for (var i = 1; i < this.count; i += 2) {
+          list.push(i)
+        }
+        return list
+      },
       message: 'Test Page',
       seen: true,
       todos: [
@@ -48,7 +81,14 @@ export default {
         { text: 'Golang' },
         { text: 'C++' }
       ],
-      cnt: 0
+      cnt: 0,
+      data: {
+        bird: 'raven',
+        animal: 'tiger',
+        fish: 'tuna',
+        proj: 'cnn',
+        major: 'EE'
+      }
     }
   },
   methods: {
